116ea8d6e31d14554102646563562b015a52b071
Vini-BM <vinicius.bayne@ufrgs.br>
2023-06-29 12:13:50 -0300

    Atualização

--- ex10_mc.py
+++ ex10_mc.py
@@ -1,35 +1,47 @@
 import matplotlib.pyplot as plt
-import random as r
+import random as rand
+import numpy as np
 
 # Funções
 
 def integrando(x,gamma):
     return x**gamma
 
-def amostragem_direta(N,funcao,gamma=0.8,seed=''):
-    r.seed(seed)
+def amostragem_direta(N,funcao,gamma,zeta,seed=''):
+    rand.seed(seed)
     gp, gn = gamma, -1*gamma
     gp_list, gn_list, n_list = [], [], []
     gp_analitico, gn_analitico = 1/(1+gp), 1/(1+gn)
     i_gp, i_gn = 0, 0
+    const = 1/(1+zeta)
     for j in range(1,int(N+1)):
-        x = r.random()
-        i_gp += funcao(x,gp)
-        i_gn += funcao(x,gn)
+        r = rand.random()
+        x = r**const
+        i_gp += funcao(x,gp-zeta)
+        print(i_gp)
+        i_gn += funcao(x,gn-zeta)
+        print(i_gn)
         gp_list.append(i_gp/j)
         gn_list.append(i_gn/j)
         n_list.append(j)
-    return n_list, gp_list, gn_list, gp_analitico, gn_analitico, gamma
+    gp_list = np.asarray(gp_list)*const
+    gn_list = np.asarray(gn_list)*const
+    return n_list, gp_list, gn_list, gp_analitico, gn_analitico
 
 # Simulação
 N = 1e5
-n, int_p, int_n, p_analitico, n_analitico, gamma = amostragem_direta(N,integrando)
+gamma = 0.2
+zeta = -0.2
+n, int_p, int_n, p_analitico, n_analitico = amostragem_direta(N,integrando,gamma,zeta)
+
+#print(int_p)
+#print(int_n)
 
 # Gráfico para gamma positivo
 plt.plot(n, int_p, label='Integral numérica', color='darkred', lw=1)
 plt.hlines(p_analitico, xmin=0, xmax=N, label='Integral analítica', color='darkgreen', lw=1.5)
-plt.xlim(0,N)
-plt.ylim(0.5,0.6)
+#plt.xlim(0,100)
+#plt.ylim(0.5,0.6)
 plt.title('Convergência da integral por amostragem direta')
 plt.xlabel('$N$')
 plt.ylabel('$I({})$'.format(gamma))
@@ -38,9 +50,9 @@ plt.show()
 
 # Gráfico para gamma negativo
 plt.plot(n, int_n, label='Integral numérica', color='darkred', lw=1)
-plt.hlines(n_analitico, xmin=0, xmax=N, label='Integral analítica', color='darkgreen', lw=1.5)
-plt.xlim(0,N)
-plt.ylim(4.2,5.2)
+#plt.hlines(n_analitico, xmin=0, xmax=N, label='Integral analítica', color='darkgreen', lw=1.5)
+#plt.xlim(0,100)
+#plt.ylim(4.2,5.2)
 plt.title('Convergência da integral por amostragem direta')
 plt.xlabel('$N$')
 plt.ylabel('$I({})$'.format(-1*gamma))
