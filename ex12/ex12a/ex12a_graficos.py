2d315e3f2563a5a57711265633bfb0b336b67710
Vini-BM <vinicius.bayne@ufrgs.br>
2023-07-15 16:42:45 -0300

    Diretórios separados para cada item, exercício a pronto, dados do exercício b prontos

---
+++ ex12/ex12a/ex12a_graficos.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+
+# --------- Leitura dos dados ---------
+cam_list = glob('walker*.txt') # lista de arquivos dos caminhantes
+tlist, xlist, ylist, sdlist = [], [], [], []
+for file in cam_list:
+    t, x, y = np.loadtxt(file,unpack=True)
+    sd = x**2 + y**2
+    xlist.append(x)
+    ylist.append(y)
+    tlist.append(t)
+    sdlist.append(sd)
+
+
+# --------- Gráficos ---------
+
+# Atualizar fonte do LaTeX
+plt.rcParams.update({
+    'font.family': 'serif',
+    'mathtext.fontset': 'cm'
+})
+
+# 1 caminhante:
+x, y, sd = xlist[0], ylist[0], sdlist[0] # primeiro da lista
+fig, axes = plt.subplots(nrows=1,ncols=2) # subplots
+fig.set_size_inches(12,6)
+ax = axes[0] # gráfico da esquerda
+ax.plot(x,y) # deslocamento
+ax.set_title('Trajetória do caminhante')
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$')
+ax = axes[1] # gráfico da direita
+ax.plot(sd,color='darkred') # desvio quadrático
+ax.set_title('Deslocamento quadrático da caminhada')
+ax.set_xlabel('$t$')
+ax.set_ylabel('$R(t)^2$')
+plt.suptitle('Caminhada aleatória de um caminhante')
+plt.subplots_adjust(wspace=0.35)
+plt.savefig('grafico_1walker.png', dpi=1500)
+plt.show()
+
+# Todos os caminhantes:
+msd = np.mean(sdlist, axis=0)
+fig, axes = plt.subplots(nrows=1,ncols=2) # subplots
+fig.set_size_inches(12,6)
+ax = axes[0] # gráfico da esquerda
+for x, y in zip(xlist,ylist):
+    ax.plot(x,y)
+ax.set_title('Trajetória dos caminhantes')
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$')
+ax = axes[1] # gráfico da direita
+ax.plot(msd,color='darkred') # desvio quadrático
+# Reta:
+dy = msd[-1]-msd[0]
+dx = len(tlist[0])
+alpha = dy/dx
+yreta = tlist[0]*alpha + msd[0]
+ax.plot(yreta,color='darkgreen',ls='--')
+ax.set_title('Deslocamento quadrático médio das caminhadas')
+ax.set_xlabel('$t$')
+ax.set_ylabel('$\\overline{{R(t)}}^2$')
+plt.suptitle(f'Caminhada aleatória de ${len(xlist)}$ caminhantes')
+plt.subplots_adjust(wspace=0.35)
+plt.savefig(f'grafico_{len(xlist)}walkers.png', dpi=1500)
+plt.show()
---
+++ ex12/ex12b/ex12b.py
@@ -0,0 +1,105 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+from random import randint
+
+# ---------- Definindo caminhante ----------
+
+class lattice_walker():
+    def __init__(self,ident,lattice_neighbor):
+        self.id, self.rede = ident, lattice_neighbor
+        self.L = int(np.sqrt(len(lattice_neighbor)))
+        self.site = randint(0,self.L**2-1) # sítio inicial (randint é inclusivo nos extremos, então o limite deve ser L**2 - 1)
+        self.x, self.y = self.site%self.L, self.site//self.L # coordenadas do sítio inicial, x é o módulo da divisão do sítio por L e y é o resultado inteiro da divisão do sítio por L (pela construção da matriz)
+    def move(self):
+        r = randint(1,4) # escolha da direção (1-direita, 2-abaixo, 3-esquerda, 4-acima)
+        self.site = self.rede[self.site][r] # nova posição é o vizinho na direção sorteada
+        self.x, self.y = self.site%self.L, self.site//self.L
+
+# ---------- Criando a matriz de vizinhança ----------
+
+def make_lattice(L):
+    N = L**2 # número de sítios
+    
+    # ---------- MATRIZ DE VIZINHANÇA ----------
+    vizinhanca = np.zeros((N,5)) # inicializa a matriz
+    
+    # ---------- Notação ----------
+    # Sentido horário:
+        # [i][0] índice
+        # [i][1] direita
+        # [i][2] abaixo
+        # [i][3] esquerda
+        # [i][4] acima
+
+    # ---------- Loop para criar a vizinhança ----------
+    for i in range(N):
+    
+        # ---------- Índice ----------
+        vizinhanca[i][0] = i
+        
+        # ---------- Direita ----------
+        # múltiplo de L subtraído de 1
+        # ------------------------------
+        if i%L == L-1: # última coluna -> ccp -> descontar L-1
+            vizinhanca[i][1] = i+1-L # vai para a primeira coluna
+        else: # demais colunas -> somar 1
+            vizinhanca[i][1] = i+1
+
+        # ---------- Abaixo ----------
+        # índice maior ou igual do que L**2 - L
+        # ------------------------------
+        if i >= N-L: # última linha -> ccp -> módulo L
+            vizinhanca[i][2] = i%L # vai para a primeira linha
+        else: # demais linhas -> somar L
+            vizinhanca[i][2] = i+L
+        
+        # ---------- Esquerda ----------
+        #         múltiplos de L
+        # ------------------------------
+        if i%L == 0: # primeira coluna -> ccp -> somar L-1
+            vizinhanca[i][3] = i+L-1 # vai para a última coluna
+        else: # demais colunas -> descontar 1
+            vizinhanca[i][3] = i-1
+        
+        # ---------- Acima ----------
+        #     índice menor do que L
+        # ------------------------------
+        if i<L: # primeira linha -> ccp -> somar N-L
+            vizinhanca[i][4] = i-L+N # vai para a última linha
+        else: # demais linhas -> descontar L
+            vizinhanca[i][4] = i-L
+    
+    # ---------- MATRIZ DA REDE ----------
+    # Criei a matriz que representa a rede, mas ela não é necessária para a simulação
+    # Deixei o código aqui caso seja preciso em outra aplicação
+    lattice_matrix = np.zeros((L,L))
+    for x in range(L):
+        for y in range(L):
+            lattice_matrix[y][x] = x + L*y # a matriz é transposta para reproduzir corretamente a vizinhança
+            
+    vizinhanca = vizinhanca.astype('int')
+    lattice_matrix = lattice_matrix.astype('int')
+    return vizinhanca, lattice_matrix
+
+
+# ---------- Simulação ----------
+
+# Parâmetros:
+L = 20 # tamanho da rede
+viz, lat = make_lattice(L) # matriz de vizinhança, matriz da rede
+num_walkers = 50
+# Inicialização:
+camlist = [lattice_walker(i,viz) for i in range(num_walkers)]
+#sitelist_global, xlist_global, ylist_global
+for cam in camlist:
+    output = open(f'latticewalker{cam.id:02}.txt', 'w')
+    output.write(f'# caminhante aleatório em matriz {L}x{L} \n')
+    output.write('# tempo  x   y   sítio \n')
+    site0, x0, y0 = cam.site, cam.x, cam.y
+    output.write(f'0    {x0}    {y0}    {site0} \n')
+    tf = 10000
+    for t in range(1,tf):
+        cam.move()
+        output.write(f'{t}    {cam.x}    {cam.y}    {cam.site} \n')
+    output.close()
