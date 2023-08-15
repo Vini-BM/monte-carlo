7d2387791a37850d280950b5f2d93467a958f0e6
Vini-BM <vinicius.bayne@ufrgs.br>
2023-08-15 20:00:29 -0300

    Correção dos scripts e funções

--- functions/random-walker/random_walkers.py
+++ functions/random-walker/random_walkers.py
@@ -1,21 +1,42 @@
 import numpy as np
 import random as rd
 
-# ---------- Caminhante aleatório na rede ----------
+# ---------- Caminhante aleatório ----------
+class walker():
+    def __init__(self,walkerseed,step=1):
+        self.id, self.passo = walkerseed, step # identifica o caminhante pela seed e define o tamanho do passo (1 por padrão)
+        self.x, self.y = 0, 0 # posição inicial
+        rd.seed(self.id)
+    def move(self):
+        # direita: x+=1 -- 1
+        # abaio: y-=1 -- 2
+        # esquerda: x-=1 -- 3
+        # acima: y+=1 -- 4
+        r = rd.randint(1,4)
+        if r == 1:
+            self.x += self.passo
+        elif r == 2:
+            self.y -= self.passo
+        elif r == 3:
+            self.x -= self.passo
+        else:
+            self.y += self.passo
 
+# ---------- Caminhante aleatório na rede ----------
 class lattice_walker():
     def __init__(self,lattice,walkerseed):
         self.id, self.rede = walkerseed, lattice # identidade do caminhante (seed) e matriz onde ele caminha
+        rd.seed(self.id)
         self.N = len(lattice)
         self.L = int(np.sqrt(self.N)) # tamanho da rede
         #self.occupation = np.zeros(self.N) # matriz de ocupação
         self.site = rd.randint(0,self.L**2-1) # sítio inicial (randint é inclusivo nos extremos, então o limite deve ser L**2 - 1)
         # x é o módulo da divisão do sítio por L e y é o resultado inteiro da divisão do sítio por L (pela construção da matriz)
         self.x, self.y = 0, 0
-        rd.seed(self.id)
     def move(self):
         r = rd.randint(1,4) # escolha da direção (1-direita, 2-abaixo, 3-esquerda, 4-acima)
-        self.site = self.rede[self.site][r] # nova posição é o vizinho na direção sorteada
+        newsite = self.rede[self.site][r] # nova posição é o vizinho na direção sorteada
+        self.site = newsite
         if r == 1:
             self.x += 1
         elif r == 2:
@@ -26,7 +47,6 @@ class lattice_walker():
             self.y -= 1
 
 # ---------- Caminhante aleatório do tipo SAW ----------
-
 class saw_walker(lattice_walker):
     def __init__(self,lattice,walkerseed):
         lattice_walker.__init__(self,lattice,walkerseed)
@@ -56,4 +76,3 @@ class saw_walker(lattice_walker):
             self.y -= 1
         self.site = newsite
         return stop
-
--- functions/scripts/sample_mean.py
+++ functions/scripts/sample_mean.py
@@ -6,13 +6,20 @@ import numpy as np
 Script para calcular a média dos dados de simulações.
 Independente do número de colunas dos arquivos, desde que seja comum a todos os arquivos
 Independente também do comprimento dos arquivos, realizando a média sobre os 'trechos' corretos dos dados
+Seleciona as colunas dos arquivos para realizar a média
 '''
 
 # Entrada
+# Path
 path = input('Caminho para os arquivos: ') # path para os arquivos relativo ao caminho desse script
 os.chdir(path)
+# Nome dos arquivos
 name_pattern = input('Nome dos arquivos (insira wildcard): ') # padrão do nome dos arquivos (espera que arquivos sejam diferenciados por um identificador como uma seed)
-output_name = name_pattern.split(sep='*')[0] + '_mean.txt' # nome do arquivo de saída
+output_name = 'mean_' + name_pattern.split(sep='*')[0] + '.txt' # nome do arquivo de saída
+# Selecionar colunas
+columns = input('Colunas para usar (vazio se todas): ')
+columns = columns.split()
+cols = [int(i) for i in columns]
 
 # Arquivos
 files = glob(f'{name_pattern}') # lista de arquivos
@@ -21,32 +28,41 @@ num_files = len(files) # número de arquivos
 # Cabeçalho
 with open(files[0], 'r') as sample:
     header = sample.readlines()[0] # cabeçalho dos arquivos, lê o primeiro da lista porque assume que todos têm o mesmo cabeçalho
+    if len(cols) == 0:
+        new_header = header
+    else: # separa as colunas correspondentes do cabeçalho
+        header = header.split()
+        new_header = header[0] + ' '
+        for i in cols:
+            new_header += header[i+1] + ' '
+        new_header += '\n'
 
 # Dados
 data = [] # lista para os dados de todos os arquivos
 data_len = [] # lista para o tamanho de cada arquivo (linhas)
 for file in files: # loop em todos os arquivos que encaixam na descrição
-    filedata = np.loadtxt(file,encoding='latin1') # leitura dos dados
+    filedata = np.loadtxt(file,encoding='latin1',usecols=cols) # leitura dos dados
     data.append(filedata)
-    data_len.append(len(filedata))
+    data_len.append(len(filedata)) # comprimento da primeira dimensão do array
 
 # Média
-longest_len = max(data_len) # maior comprimento dos dados
-if all(length == longest_len for length in data_len): # se todos os arquivos têm o mesmo comprimento, a média é simples
+maxlen = max(data_len) # maior comprimento dos dados
+if all(length == maxlen for length in data_len): # se todos os arquivos têm o mesmo comprimento, a média é simples
     mean_data = np.mean(data, axis=0)
 else: # arquivos têm diferentes comprimentos, e a média deve considerar os trechos de comprimento comum a eles
     reshaped_data = [] # lista para novo formato dos dados
     for filedata in data: # loop sobre os dados
-        fill = maxlen - sdlen # número de elementos que falta para o array ter o comprimento da amostra mais comprida
-        fill_array = np.full(fill,np.nan) # array preenchido com NaN de comprimento igual a diferença anterior
-        reshaped_filedata = np.append(filedata,fill_array) # novo array é o array antigo com o número correto de NaN adicionado ao final
+        filedata_shape = np.shape(filedata) # dimensões do array
+        fill = maxlen - filedata_shape[0] # número de elementos que falta para o array ter o comprimento da amostra mais comprida
+        fill_array = np.full((fill,filedata_shape[1]),np.nan) # array preenchido com NaN de comprimento igual a diferença anterior, com as dimensões corretas
+        reshaped_filedata = np.append(filedata,fill_array,axis=0) # novo array é o array antigo com o número correto de NaN adicionado ao final
         reshaped_data.append(reshaped_filedata) # adicionar novo array na lista
     mean_data = np.nanmean(reshaped_data, axis=0) # a função np.nanmean realiza a média sobre os elementos numéricos, desconsiderando os NaN
 
 # Saída
 with open(output_name, 'w') as output:
     output.write(f'# {num_files} \n') # escreve número de arquivos sobre os quais a média foi tomada
-    output.write(header) # escreve cabeçalho dos arquivos
+    output.write(new_header) # escreve cabeçalho dos arquivos
     for points in mean_data: # para cada tupla nos dados
         for i in range(len(points)): # para cada elemento da tupla
             output.write(str(points[i]) + '    ') # escreve elemento
